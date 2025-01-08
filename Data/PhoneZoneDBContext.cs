31581e34bc02e135cb472e6b278b0af9ef910f4c
HieuMT249 <hieumt249@gmail.com>
2025-01-08 12:05:18 +0700

    first commit

---
+++ Data/PhoneZoneDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using phonezone_backend.Models;
+
+namespace phonezone_backend.Data
+{
+    public class PhoneZoneDBContext : DbContext
+    {
+        public PhoneZoneDBContext (DbContextOptions<PhoneZoneDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<phonezone_backend.Models.User> Users { get; set; } = default!;
+    }
+}
