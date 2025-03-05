0bc37c4c0e6a42f366a300bace729bcdd348773b
HieuMT249 <hieumt249@gmail.com>
2025-03-05 12:47:13 +0700

    done

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -10,7 +10,7 @@ using phonezone_backend.Models;
 
 namespace phonezone_backend.Controllers
 {
-    [Route("api/v1/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
